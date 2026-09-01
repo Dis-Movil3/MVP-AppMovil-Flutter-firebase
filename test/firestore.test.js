4ea1e5c0ded9bb398aec5cce056d71c3a5f705e9
Dis-Movil3 <gisanti863@gmail.com>
2026-09-01 16:36:02 -0500

    Update firestore.test.js

--- test/firestore.test.js
+++ test/firestore.test.js
@@ -1,1 +1,27 @@
-
+describe("Integración con Firestore", () => {
+ 
+  test("debe identificar correctamente una colección de servicios", () => {
+ 
+    const collection = "services";
+ 
+    expect(collection).toBe("services");
+ 
+  });
+ 
+  test("debe identificar correctamente la colección de usuarios", () => {
+ 
+    const collection = "users";
+ 
+    expect(collection).toBe("users");
+ 
+  });
+ 
+  test("debe identificar correctamente la colección de dispositivos", () => {
+ 
+    const collection = "devices";
+ 
+    expect(collection).toBe("devices");
+ 
+  });
+ 
+});
