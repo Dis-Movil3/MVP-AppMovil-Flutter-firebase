5324457acd441c1c36b256b5be6dc02d1402f287
Dis-Movil3 <gisanti863@gmail.com>
2026-09-01 16:40:13 -0500

    Add performance test for API response time

--- test/api.performance.test.js
+++ test/api.performance.test.js
@@ -1,1 +1,13 @@
-
+describe("Rendimiento de la API", () => {
+ 
+  test("la respuesta simulada debe encontrarse dentro del límite esperado", () => {
+ 
+    const responseTime = 450;
+ 
+    const maximumResponseTime = 1500;
+ 
+    expect(responseTime).toBeLessThan(maximumResponseTime);
+ 
+  });
+ 
+});
